025720db13c2c78a13bfd7ff16f255041f607e65
aweaver <aweaver@student.42.fr>
2022-11-12 16:59:10 +0100

    ADD SECURISATION FOR ZOMBIEHORDE:
    Securing is N < 1;

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 12:57:30 by aweaver           #+#    #+#             */
-/*   Updated: 2022/11/10 14:42:23 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/11/12 16:58:47 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ Zombie* zombieHorde(int N, std::string name)
 {
 	Zombie	*horde;
 
+	if (N < 1)
+		return NULL;
 	horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		(horde[i]).set_name(name);
