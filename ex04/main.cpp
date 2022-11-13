25668f494b36e107bcec91205edcf6d43477cb7d
aweaver <aweaver@student.42.fr>
2022-11-13 11:22:13 +0100

    UPDATE EX04:
    Adding Makefile rule to check for normal file toto.txt
    
    Adding .gitignore rule.
    Adding checks if infile is a folder.

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,8 +6,40 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 17:12:19 by aweaver           #+#    #+#             */
-/*   Updated: 2022/11/12 17:12:20 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/11/13 11:20:54 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <fstream>
 
+int	main(int argc, char **argv)
+{
+	std::string		buffer;
+	std::ifstream	inputfile;
+	std::ofstream	outputfile;
+	std::string		outputname;
+
+	if (argc != 4)
+	{
+		std::cerr << "Usage <filename> <string_to_search> <string_to_replace>"
+			<< std::endl;
+		return (0);
+	}
+	inputfile.open(argv[1], std::ifstream::in);
+	if (inputfile.peek() == EOF)
+	{
+		std::cerr << "Invalid file given" << std::endl;
+		return (0);
+	}
+	outputname = argv[1];
+	outputfile.open(outputname.append(".replace"), std::ofstream::out);
+	while (!inputfile.eof())
+	{
+		std::getline(inputfile, buffer);
+		std::cout << buffer << std::endl;
+	}
+	inputfile.close();
+	return (0);
+}
