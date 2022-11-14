d2b6862e6e99a7ccebd6e9307de4612e2bb7e862
Adrian WEAVER <aweaver@student.42.fr>
2022-11-14 14:15:48 +0100

    UPDATE MAIN EX05:
    Adding a doesn't exist test case.

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:16:33 by aweaver           #+#    #+#             */
-/*   Updated: 2022/11/14 12:13:24 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/11/14 13:30:54 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	harl.complain(argv[1]);
+	harl.complain("this doesn't exist");
 	harl.complain("Debug");
 	harl.complain("DEBUG");
 	harl.complain("ERROR");
